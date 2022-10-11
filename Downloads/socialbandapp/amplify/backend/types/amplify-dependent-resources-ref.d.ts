442040e0500122f22a822e4a733566991e6f446d
Aditya Kumar <adityaverma.dev@gmail.com>
2022-10-11 23:13:22 +0530

    check

---
+++ Downloads/socialbandapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "socialbandapp1204f87e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
